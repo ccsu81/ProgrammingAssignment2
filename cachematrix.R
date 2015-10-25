c7a3445c982e4942ee2dbd213de99216a6f68c49
ccsu81 <ccsu@ucdavis.edu>
2015-10-25 16:49:49 +0800

    Finished Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function take in as argument a matrix and return a list in which
+## the objects are functions and cache the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+## 1. Set the value of the Matrix
+	set <- function(y){
+			x <<- y
+			m <<- NULL
+	}
+## 2. Get the value of the vector
+	get <- function() x
+## 3. Set the value of the Inverse
+	setinverse <- function(solve) m <<- solve
+## 4. Get the value of the Inverse
+	getinverse <- function() m
+## Finally, the function return a list of objects which are fuctions.
+	list(set = set, get = get, setinverse = setinverse, 
+		getinverse = getinverse)
 }
 
+## This function computes the inverse of the special "matrix"
+## returned by 'makeCacheMatrix' above. If the inverse has already
+## been calculated (and the matrix has not changed), then 'cacheSolve'
+## should retrieve the inverse from the cache
 
-## Write a short comment describing this function
+## This function retrives the inverse from x first. If inverse exist, 
+## then the function prints out the inverse. If not, then matrix is
+## stored in 'data' and the inverse is calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Retrives 'm' from the global environment 
+## ('m' is stored globally by the function "makeCacheMatrix" with the operator '<<-')
+	m <- x$getinverse()
+## If the inverse already exist, then this function ends and prints the inverse
+	if(!is.null(m)){
+			message("getting cached data")
+			return(m)
+	}
+## If the inverse does not exist, the matrix is stored in 'data'
+	data <- x$get()
+## and using 'solve', the inverse is stored in 'm'
+	m <- solve(data)
+## using function setinverse, 
+	x$setinverse(m)
+## Return a matrix that is the inverse of 'x'
+	m
 }
